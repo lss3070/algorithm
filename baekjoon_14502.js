b66b4b6f005a45a6978780eb8558563e62b4640f
songseop.lee <lss3070@hanmail.net>
2020-11-26 23:46:46 +0900

    1126 post

--- baekjoon_14502.js
+++ baekjoon_14502.js
@@ -10,21 +10,83 @@ let input="7 7\n\
 
 let xarea=[0,1,0,-1];
 let yarea=[-1,0,1,0];
+let room;
+let length;
 
 solution();
 
 function solution(){
-    input.shift();
+   length=input.shift().split(" ").map(e=>+e)[0];
 
-    let room = input.map(e=>{
+
+    room = input.map(e=>{
        return e.split(" ").map(e=>+e);
     })  
+
+
    
     let queue=[];
     
+    for(let i=0;i<length;i++){
+        for(let j=0; j<length;i++){
+            if(room[i][j]==2&&check(i,j)){
+                queue.push([i,j]);
+               let aa= BFS(queue);
+               console.log(aa);
+            }
+        }
+    }
     
 }
 
-function BFS(){
+function DFS(x,y){
+    if(cnt==3){
+        BFS();
+    }
+    for(let i=x;i<length;i++){
+        
+    }
 
 }
+function BFS(queue){
+   let now= queue.shift();
+   let result;
+
+   for(let i=0;i<xarea.length;i++){
+        let dX=  now[0]+xarea[i];
+        let dY = now[1]+yarea[i];
+
+        if(check(dX,dY)){
+            if(room[dX][dY]==0&&!QueCheck(queue,dX,dY)){
+                room[dX][dY]=2;
+                queue.push([dX,dY]);
+               result= BFS(queue);
+            }
+        }
+  
+   }
+
+   if(queue.length==0){
+        result= room.reduce((pre,cur)=>{
+          return pre+(cur.filter(e=>e==0).length);
+        },0)
+   }
+
+   return result;
+}
+
+
+function check(x,y){
+    if((x>=0)&&(y>=0)&&(x<length)&&(y<length)){
+        return true
+    }else return false;
+}
+
+
+function QueCheck(queue,x,y){
+    queue.forEach(e=>{
+        if(e[0]==x&&e[1]==y) return true;
+    })
+    return false;
+}
+
--- baekjoon_2178.js
+++ baekjoon_2178.js
@@ -1,7 +1,79 @@
+let input ="4 6\n\
+101111\n\
+101010\n\
+101011\n\
+111011".split("\n");
 
+
+let xMo=[0,1,0,-1];
+let yMo=[-1,0,1,0];
+
+let length;
+let result;
+
+let end;
 
 solution();
 
 function solution(){
+    end= input.shift().split(" ").map(e=>+e);
 
+
+   input= input.map(e=>e.split("").map(e=>+e));
+   length=[input.length-1,input[0].length-1];
+
+    let queue =[]
+    queue.push([0,0]);
+
+    result= BFS(queue,[],1);
+    console.log(result);
 }
+
+function BFS(queue,visitlist,depth){
+    let result=999;
+    queueNext=[];
+
+while(queue.length>0){
+    let now= queue.shift();
+    visitlist.push(now);
+    if(now[0]==end[0]&&now[1]==end[1]){
+     result = Math.min(r,depth);
+     break;
+    }
+ 
+     for(let i=0;i<xMo.length;i++){
+         let dX= now[0]+xMo[i];
+         let dY= now[1]+yMo[i];
+ 
+         if(overcheck(dX,dY)&&queueCheck(visitlist,dX,dY)){
+             if(queueCheck(queue,dX,dY)&&input[dX][dY]==1){
+                 queueNext.push([dX,dY]);
+             }
+         }
+     }
+}
+
+
+    if(queueNext.length>0){
+        result = Math.min(result,BFS(queueNext,visitlist,depth+1));
+    }
+    return result;
+}
+
+function overcheck(x,y){
+    if(x>=0&&y>=0&&x<length[0]&&y<length[1]){
+        return true;
+    }else return false;
+}
+
+function queueCheck(queue,x,y){
+
+    
+    queue.forEach(e => {
+        if(e[0]===x&&e[1]===y){
+            return false;
+        }
+    });
+    return true;
+}
+
